e9638aac716a9aeade444969bf1cc5bf004c579f
RiptideWallace <brysonc1987@gmail.com>
2017-07-03 21:58:54 +0000

    up to step 3 of the implement getRepoContributors assignment in the avatar downloader project

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  getRepoContributors("https://github.com/nodejs/node", function (err, result) {
+    console.log("Errors: ", err);
+    console.log("Result: ", result);
+  });
+
+}
+
 console.log('Welcome to the GitHub Avatar Downloader!');
